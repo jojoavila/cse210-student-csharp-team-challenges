4ce7350b4d378316f2f4cc7391a7dbe7d1767d5e
jojoavila <avilajosuealejandro@gmail.com>
2021-10-15 17:19:45 -0600

    User_service class

--- 05-jumper/User_Service.cs
+++ 05-jumper/User_Service.cs
@@ -4,6 +4,12 @@ namespace _05_jumper
 {
     public class User_Service
     {
-        
+        string UserGuess()
+        {
+            Console.WriteLine($"Guess a letter [a-z]: ");
+            string guess = Console.ReadLine();
+            
+            return guess;
+        }
     }
 }
