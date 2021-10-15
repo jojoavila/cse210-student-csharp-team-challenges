b149815fbb0ea635581f52c4f938d93c51c39398
chvia223 <via17001@byui.edu>
2021-10-15 11:09:32 -0600

    added initial class files

--- 05-jumper/Program.cs
+++ 05-jumper/Program.cs
@@ -6,7 +6,10 @@ namespace _05_jumper
     {
         static void Main(string[] args)
         {
+            Board board = new Board();    
             Console.WriteLine("Starting point for the Jumper project.");
+            board.generateBoard();
+            board.displayBoard();
         }
     }
 }
