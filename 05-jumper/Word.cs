d002ba7ffe610377f3e8919e5e74cf6628359cc4
chvia223 <via17001@byui.edu>
2021-10-16 14:54:04 -0600

    added word instializing functions

--- 05-jumper/Word.cs
+++ 05-jumper/Word.cs
@@ -17,10 +17,14 @@ namespace _05_jumper
             "cow",
             "chicken"
         };
+
+        public string _word;
+        public string _coveredWord;
         
         public Word()
         {
-            
+            _word = RandomWord();
+            _coveredWord = GetCoveredWord();
         }
 /// <summary>
 /// Pulls from the list of words from the class, then chooses one at random
@@ -32,5 +36,19 @@ namespace _05_jumper
             string randomWord = (_words[r.Next(0, _words.Count)]);
             return randomWord;
         }
+
+        public string GetCoveredWord()
+        {
+            string coveredWord = _word;
+
+            for (int i = 0; i < coveredWord.Length; i++)
+            {
+                coveredWord = coveredWord.Remove(i, 1).Insert(i, "_");
+            }
+            
+            return coveredWord;        
+        }
+
+
     }
 }
