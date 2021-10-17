2a90653c5d8d95ce615eb75efb6b2aa583042a31
chvia223 <via17001@byui.edu>
2021-10-17 17:14:10 -0600

    added jumper methods

--- 05-jumper/Jumper.cs
+++ 05-jumper/Jumper.cs
@@ -4,19 +4,52 @@ namespace _05_jumper
 {
     public class Jumper
     {
+        public int _wrongGuesses;
+        public int _rightGuesses;
+
         public Jumper()
         {
-
+            _wrongGuesses = 0;
+            _rightGuesses = 0;
         }
 
-        public void CheckLetter()
+        public bool CheckLetter(string word, char guess)
         {
-
+            int pos = 0;
+                foreach (char letter in word)
+                {
+                    if (letter == guess)
+                    {
+                        _rightGuesses++;
+                        return true;
+                    }
+                    pos++;
+                }
+                _wrongGuesses++;
+                return false;
         }
 
-        public void IsAlive()
+        public int GetWrongGuesses()
         {
-            
+            return _wrongGuesses;
+        }
+
+        public int GetRightGuesses()
+        {
+            return _rightGuesses;
+        }
+
+
+        public bool IsAlive()
+        {
+            if (_wrongGuesses == 4)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
