1cb6afb0727c80f99faa95ea969afa1d6490038b
chvia223 <via17001@byui.edu>
2021-10-17 17:14:36 -0600

    added userService methods

--- 05-jumper/User_Service.cs
+++ 05-jumper/User_Service.cs
@@ -5,5 +5,28 @@ namespace _05_jumper
     public class User_Service
     {
         
+
+
+        public char GetLetterGuess(string promptText)
+        {
+            Console.Write(promptText);
+            string userInput = Console.ReadLine();
+
+            // Converts 
+            var letter = userInput[0];
+            return letter;
+        }
+
+        public void EndMessage(bool isAlive, string word, string coveredWord)
+        {
+            if (isAlive == false) 
+            {
+                Console.WriteLine("Oh no! You lost. Thanks for playing.");
+            }
+            else if (word == coveredWord)
+            {
+                Console.WriteLine("Congrats! You won!");
+            }
+        }
     }
 }
