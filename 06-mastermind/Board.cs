ec90c466214f015ad6deddaf5305df5391848342
Tyler Skalka <tyler.skalka@gmail.com>
2021-10-23 21:01:01 -0600

    comments added, cosmetic changes. The code still works

--- 06-mastermind/Board.cs
+++ 06-mastermind/Board.cs
@@ -2,16 +2,32 @@ using System;
 
 namespace _06_mastermind
 {
+
+    /// <summary>
+    ///This class stores the secret code
+    ///it's also responsible for creating a new board.
+    /// also it compares the secret code to userguess to give current hint
+    /// determins if game is over
+    /// </summary>
     class Board
     {
         string _secretCode; 
         UserService _userService = new UserService();
    
+
+        /// <summary>
+        /// This class will initialize the secret code, and make it a string.
+        /// </summary>
         public Board()
         {
             RandomCode();
         }
 
+
+        /// <summary>
+        /// This function will generate a new random code and convert the random secret code to  a string.
+        /// Also stores a debug print if needed.
+        /// </summary>
         public void RandomCode()
         {
             Random randNum = new Random();
@@ -20,9 +36,16 @@ namespace _06_mastermind
             _secretCode = num.ToString();
 
             // Debug code --> shows secret code before guessing starts
-            Console.WriteLine(_secretCode);
+            // Uncomment if debug needed.
+            // Console.WriteLine(_secretCode);
         }
 
+        /// <summary>
+        /// This function takes in a the guess and copmares it to the secret code
+        /// first it seese if the spot is correct then updates with an X
+        /// or it just continues to see if there is any part in the code at all and adds 0
+        /// returns the new hint string
+        /// </summary>
         public string GetHint(string guess)
         {
             string hint = "****";
@@ -43,6 +66,10 @@ namespace _06_mastermind
             return hint;
         }    
 
+        /// <summary>
+        /// This function takes in the player 1 and 2 information
+        /// prints out the board with the relavent player information
+        /// </summary>
         public void DisplayBoard(Player player1, Player player2)
         {
             Console.WriteLine("");
@@ -52,6 +79,11 @@ namespace _06_mastermind
             Console.WriteLine("--------------------");
         }
 
+
+        /// <summary>
+        /// Determins if player guess equals the scecret code 
+        /// returns bool true if game is over
+        /// </summary>
         public bool IsWin(string guess)
         {
             bool win = false;
