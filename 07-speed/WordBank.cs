a617da4bfc0f34edc796842e47daa7b7aa38680f
chvia223 <via17001@byui.edu>
2021-11-05 13:57:27 -0600

    added initialization function and commented out unused code

--- 07-speed/WordBank.cs
+++ 07-speed/WordBank.cs
@@ -9,73 +9,75 @@ namespace _07_speed
     /// </summary>
     public class WordBank
     {
-
-        private List<string> _words = new List<string> 
-        {
-            "person",
-            "dog",
-            "cart",
-            "chicken",
-            "house",
-            "car",
-            "laptop",
-            "computer",
-            "programming",
-            "refridgerator",
-            "notebook",
-            "light",
-            "onion",
-            "pen",
-            "pencil",
-            "world",
-            "power",
-            "darkness",
-        };
-
-        private List<string> _wordsToRemove = new List<string>{};
-
-        private string _randWord;
-
+        private List<string> _wordList = new List<string>();
 
         public WordBank()
         {
-
+            InitializeWordList();
         }
         
-        private void RemoveWord()
+        /// <summary>
+        /// Prepares a word bank to be used when
+        /// creating new Word objects.
+        /// </summary>
+        private void InitializeWordList()
         {
-
-            foreach(string w in _words)
+            // This may seem redundant now, but if it is
+            // decided later on to pull a list of words
+            // from a file then this is way we'll do that.
+            // For now this function is just a place holder.
+            List<string> wordList = new List<string>
             {
-                //buffer.match == word
-                if(true)
-                {
-                    _wordsToRemove.Add(w);
-                }
-            }
-            foreach(string removalWord in _wordsToRemove)
-            {
-                _words.Remove(removalWord);
+                "person",
+                "dog",
+                "cart",
+                "chicken",
+                "house",
+                "car",
+                "laptop",
+                "computer",
+                "programming",
+                "refridgerator",
+                "notebook",
+                "light",
+                "onion",
+                "pen",
+                "pencil",
+                "world",
+                "power",
+                "darkness"
+            };
 
-            }
-            
-            // more changes
-            // how to remove from list, for each word in list of words
-            // if (buffer.match)
-            // _wordsToRemve.add(w)
-
-        // add the list into a new list,
-        // go through new list and remove them from the old list
-        // for each word removalWord in words to remove
-        // _words.remove(removal word)
-        // random number 0-1, 
+            _wordList = wordList;
         }
+
+        // The Word object is suppose to be removed from the
+        // Word object List declared in the Director class, not
+        // a word from the list of available words to choose from.
+        //
+        // private void RemoveWord()
+        // {
+
+        //     foreach(string w in _words)
+        //     {
+        //         //buffer.match == word
+        //         if(true)
+        //         {
+        //             _wordsToRemove.Add(w);
+        //         }
+        //     }
+        //     foreach(string removalWord in _wordsToRemove)
+        //     {
+        //         _words.Remove(removalWord);
+
+        //     }
+        // }
 
         public string GetRandomWord()
         {
             Random randomGenerator = new Random();
 
-            return _words[randomGenerator.Next(0,_words.Count)];
+            return _wordList[randomGenerator.Next(0,_wordList.Count)];
         }
     }
 }
